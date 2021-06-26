143ebf3c536ff2f407a9d75ecc37ea726b07ade6
kiszewski <kiszewski1999@gmail.com>
2021-06-26 18:51:58 -0300

    Add BASE_URL constant to all project

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -3,12 +3,13 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:github_search_repos/app/modules/repository_webview/repositoryWebview_module.dart';
 import 'package:github_search_repos/app/modules/user_repositories/userRepositories_module.dart';
 
+import 'core/constants.dart';
 import 'modules/home/home_module.dart';
 
 class AppModule extends Module {
   @override
   final List<Bind> binds = [
-    Bind.singleton((i) => Dio(BaseOptions(baseUrl: 'https://api.github.com')))
+    Bind.singleton((i) => Dio(BaseOptions(baseUrl: BASE_URL)))
   ];
 
   @override
