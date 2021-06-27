a29895b5f05f633ec2eb5eac96a98ebcdd0f25d0
kiszewski <kiszewski1999@gmail.com>
2021-06-27 16:41:48 -0300

    Remove mockito import

--- test/app/modules/home/repositories/home_repository_test.dart
+++ test/app/modules/home/repositories/home_repository_test.dart
@@ -3,11 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:github_search_repos/app/core/constants.dart';
 import 'package:github_search_repos/app/modules/home/repositories/home_repository.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
-import 'package:mockito/mockito.dart';
 
 import 'success_response_mock.dart';
-
-class ClientMock extends Mock implements Dio {}
 
 void main() {
   late final Dio client;
